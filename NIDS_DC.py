eb430e55f4f181113551c8f40c6099c3338d1d41
Dave Cameron <dave@tinyweb.ca>
2024-02-17 14:06:00 -0800

    Working on analysis and hyperparams

--- NIDS_DC.py
+++ NIDS_DC.py
@@ -1,12 +1,193 @@
 #!/usr/bin/env python3
+import numpy as np
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-from sklearn import metrics
+import pydotplus
+import seaborn as sns
 import sys
+import time
+from io import StringIO
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split, cross_val_score, RandomizedSearchCV
+from sklearn.tree import export_graphviz
+from sklearn import metrics
+from IPython import display
 
-FEATURES_TO_USE = 10
+
+# TODO - In Progress
+def advanced_analysis(X_train, X_test, y_train, y_test):
+    # Number of trees in random forest
+    n_estimators = np.linspace(100, 3000, int((3000 - 100) / 200) + 1, dtype=int)
+    # Number of features to consider at every split
+    max_features = ['auto', 'sqrt']
+    # Maximum number of levels in tree
+    max_depth = [1, 5, 10, 20, 50, 75, 100, 150, 200]
+    # Minimum number of samples required to split a node
+    min_samples_split = [1, 2, 5, 10, 15, 20, 30]
+    # Minimum number of samples required at each leaf node
+    min_samples_leaf = [1, 2, 3, 4]
+    # Method of selecting samples for training each tree
+    bootstrap = [True, False]
+    # Criterion
+    criterion = ['gini', 'entropy']
+    random_grid = {'n_estimators': n_estimators,
+                   'max_features': max_features,
+                   'max_depth': max_depth,
+                   'min_samples_split': min_samples_split,
+                   'min_samples_leaf': min_samples_leaf,
+                   'bootstrap': bootstrap,
+                   'criterion': criterion}
+
+    rf_base = RandomForestClassifier()
+    rf_random = RandomizedSearchCV(estimator=rf_base, param_distributions=random_grid, n_iter=30, cv=5, verbose=2,
+                                   random_state=42, n_jobs=4)
+    rf_random.fit(X_train, y_train)
+    print(rf_random.score(X_train, y_train))
+    print(rf_random.score(X_test, y_test))
+
+    # Part Two
+    # Create tree
+    estimator = clf.estimators_[2]
+    class_names = np.array(df['attack_cat'].unique())
+
+    dot_data = StringIO()
+    export_graphviz(estimator, out_file=dot_data, class_names=class_names, rounded=True, proportion=False, precision=2,
+                    filled=True)
+
+    graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+    graph.write_png('tree.png')
+    display.Image(graph.create_png())
+
+
+# ************************* Generation ************************* #
+def create_model(filename):
+    print(f'Loading data from {filename}')
+    df = pd.read_csv(filename, header=0, low_memory=False, skipinitialspace=True)
+    df['attack_cat'] = df['attack_cat'].str.strip()
+    print("Dataset loaded\n")
+
+    # Transform object columns
+    # sport / dsport are really categorical, not numeric
+    # Booleans are treated as categorical
+    df['srcip'], _ = pd.factorize(df['srcip'])
+    df['sport'], _ = pd.factorize(df['sport'])
+    df['dstip'], _ = pd.factorize(df['dstip'])
+    df['dsport'], _ = pd.factorize(df['dsport'])
+    df['proto'], _ = pd.factorize(df['proto'])
+    df['state'], _ = pd.factorize(df['state'])
+    df['service'], _ = pd.factorize(df['service'])
+    df['ct_flw_http_mthd'], _ = pd.factorize(df['ct_flw_http_mthd'])
+    df['is_ftp_login'], _ = pd.factorize(df['is_ftp_login'])
+    df['ct_ftp_cmd'], _ = pd.factorize(df['ct_ftp_cmd'])
+    df['attack_cat'] = df['attack_cat'].fillna('')
+    df['attack_cat'] = df['attack_cat'].astype('string')
+    df = df.replace('Backdoor', 'Backdoors')
+    df['Label'] = df['Label'].astype(bool)
+
+    return RandomForestClassifier(n_estimators=75, criterion='entropy', max_depth=100, max_features=5), df
+
+
+# ************************* Analysis ************************* #
+def perform_analysis(df, clf, FEATURES_TO_USE=12, USE_CORRELATION=True):
+    # Analysis
+    print("Perfoming analysis")
+    analysis_set = df.copy()
+    analysis_set['attack_cat'], _ = pd.factorize(analysis_set['attack_cat'])
+
+    correlation = analysis_set.corr().values[-2:-1]
+    covariance = analysis_set.cov().values[-2:-1]
+
+    # Find the indices of the critical features
+    correlation_keys = dict(enumerate(correlation[0]))
+    # Remove label and attack_cat
+    correlation_keys.popitem()
+    correlation_keys.popitem()
+    covariance_keys = dict(enumerate(covariance[0]))
+    # Remove label and attack_cat
+    covariance_keys.popitem()
+    covariance_keys.popitem()
+
+    correlation_keys = dict(sorted(correlation_keys.items(), key=lambda x: x[1]))
+    covariance_keys = dict(sorted(covariance_keys.items(), key=lambda x: x[1]))
+
+    if USE_CORRELATION:
+        critical_keys = list(correlation_keys.keys())[FEATURES_TO_USE * -1:]
+    else:
+        critical_keys = list(covariance_keys.keys())[FEATURES_TO_USE * -1:]
+    critical_keys = np.array(critical_keys)
+    print('found critical keys: ', critical_keys)
+    X = df.iloc[:, np.r_[critical_keys]]
+    X_Temp = analysis_set.iloc[:, np.r_[critical_keys, (len(df.columns) - 2)]]
+
+    # Create PairPlot
+    plot = sns.pairplot(X_Temp.head(10000), diag_kind='kde')
+    plot.savefig("pairplot.png")
+    print("Created PairPlot")
+
+    # Create heatmap
+    heatmap = sns.heatmap(X_Temp.head(10000), annot=True, xticklabels=X.columns, yticklabels=X.columns)
+    figure = heatmap.get_figure()
+    display.Image(figure)
+    figure.savefig("heatmap.png")
+    print("Created HeatMap")
+
+    return X
+
+
+# ************************* Label Prediction ************************* #
+def classify_label(df, clf, X, RUN_CROSS_VALIDATION=False):
+    # Train label classifier
+    print("Training label classifier")
+    y = df.iloc[:, -1:]['Label'].tolist()
+
+    # Split into training and test
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1 / 5, random_state=0)
+
+    # Train model
+    clf = clf.fit(X_train, y_train)
+
+    # Predict results
+    print("Predicting label")
+    y_pred = clf.predict(X_test)
+    print("************************************************************")
+    print("Accuracy: {:.2f}%\n".format(metrics.accuracy_score(y_test, y_pred) * 100))
+    print(metrics.classification_report(y_test, y_pred))
+
+    if RUN_CROSS_VALIDATION:
+        print("************************************************************")
+        print("Analyzing cross validation")
+        cross_validation = cross_val_score(clf, X, y, cv=5, scoring='accuracy', n_jobs=4)
+        print(f'Cross Validation Score:\n{cross_validation}')
+
+    print("************************************************************\n")
+
+
+# ************************* Attack_Cat Prediction ************************* #
+def classify_attack_cat(df, clf, X, RUN_CROSS_VALIDATION=False):
+    # Train attack_cat classifier
+    print("Training attack_cat classifier")
+    y = df.iloc[:, -2:-1]['attack_cat'].tolist()
+
+    # Split into training and test
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1 / 5, random_state=0)
+
+    # Train model
+    clf = clf.fit(X_train, y_train)
+
+    # Predict results
+    print("Predicting attack_cat")
+    y_pred = clf.predict(X_test)
+    print("************************************************************")
+    print("Accuracy: {:.2f}%\n".format(metrics.accuracy_score(y_test, y_pred) * 100))
+    print(metrics.classification_report(y_test, y_pred))
+
+    if RUN_CROSS_VALIDATION:
+        print("************************************************************")
+        print("Analyzing cross validation")
+        cross_validation = cross_val_score(clf, X, y, cv=5, scoring='accuracy', n_jobs=4)
+        print(f'Cross Validation Score:\n{cross_validation}')
+
+    print("************************************************************\n")
+
 
 filename = ""
 if len(sys.argv) > 0:
@@ -14,82 +195,12 @@ if len(sys.argv) > 0:
 else:
     exit(1)
 
-# Import data
-print(f'Loading data from {filename}')
-dataset = pd.read_csv(filename, header=0, low_memory=False)
-print("Dataset loaded\n")
+start_time = time.time()
 
-# Transform object columns
-# sport / dsport are really categorical, not numeric
-# Booleans are treated as categorical
-dataset['srcip'], _ = pd.factorize(dataset['srcip'])
-dataset['sport'], _ = pd.factorize(dataset['sport'])
-dataset['dstip'], _ = pd.factorize(dataset['dstip'])
-dataset['dsport'], _ = pd.factorize(dataset['dsport'])
-dataset['proto'], _ = pd.factorize(dataset['proto'])
-dataset['state'], _ = pd.factorize(dataset['state'])
-dataset['service'], _ = pd.factorize(dataset['service'])
-dataset['ct_flw_http_mthd'], _ = pd.factorize(dataset['ct_flw_http_mthd'])
-dataset['is_ftp_login'], _ = pd.factorize(dataset['is_ftp_login'])
-dataset['ct_ftp_cmd'], _ = pd.factorize(dataset['ct_ftp_cmd'])
-dataset['attack_cat'], _ = pd.factorize(dataset['attack_cat'])
-dataset['Label'], _ = pd.factorize(dataset['Label'])
+clf, df = create_model(filename)
+X = perform_analysis(df, clf)
+# classify_label(df, clf, X)
+classify_attack_cat(df, clf, X)
 
-# ************************* Analysis ************************* #
-
-# Analysis
-X = dataset.iloc[:, :-2].values
-correlation = dataset.corr().values[-1]
-covariance = dataset.cov().values[-1]
-
-# Find the indices of the critical features
-correlation_keys = {}
-for i in range(0, len(correlation)):
-    correlation_keys[i] = correlation[i]
-covariance_keys = {}
-for i in range(0, len(covariance)):
-    covariance_keys[i] = covariance[i]
-
-correlation_keys = dict(sorted(correlation_keys.items(), key=lambda x: x[1]))
-covariance_keys = dict(sorted(covariance_keys.items(), key=lambda x: x[1]))
-
-critical_keys = list(correlation_keys.keys())[FEATURES_TO_USE*-1:]
-
-# ************************* Label Prediction ************************* #
-# Train label classifier
-print("Training label classifier")
-y = dataset.iloc[:, -1:]['Label'].tolist()
-
-# Split into training and test
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1/5, random_state=0)
-
-# Train model
-clf = RandomForestClassifier()
-clf = clf.fit(X_train, y_train)
-
-# Predict results
-y_pred = clf.predict(X_test)
-print("************************************************************")
-print("Accuracy: {:.2f}%\n".format(metrics.accuracy_score(y_test, y_pred) * 100))
-print(metrics.classification_report(y_test, y_pred))
-print("************************************************************\n")
-
-# ************************* Attack_Cat Prediction ************************* #
-
-# Train attack_cat classifier
-print("Training attack_cat classifier")
-y = dataset.iloc[:, -2:-1]['attack_cat'].tolist()
-
-# Split into training and test
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1/5, random_state=0)
-
-# Train model
-clf = RandomForestClassifier()
-clf = clf.fit(X_train, y_train)
-
-# Predict results
-y_pred = clf.predict(X_test)
-print("************************************************************")
-print("Accuracy: {:.2f}%\n".format(metrics.accuracy_score(y_test, y_pred) * 100))
-print(metrics.classification_report(y_test, y_pred))
-print("************************************************************\n")
+execution_time = time.time() - start_time
+print(f"Execution took {round(execution_time, 2)} seconds")
